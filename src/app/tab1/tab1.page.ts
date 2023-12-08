f543a392b0e22a55dbc87a3c1882418f72479ba8
Maxime De Sogus <desogus.maxime@moviplus.ch>
2023-12-08 11:50:55 +0100

    watch gps

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule],
 })
 export class Tab1Page {
-  constructor() {}
+  position$ = new Subject<Position | null>();
+
+  constructor() {
+    Geolocation.watchPosition(
+      {},
+      (position, err) => {
+        this.position$.next(position);
+      },
+    );
+  }
 }
